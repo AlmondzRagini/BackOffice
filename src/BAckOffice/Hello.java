87fc033edaa00f81ca863e550387ffdd9556b2ef
AlmondzRagini <gal.ragini@gmail.com>
2024-12-18 14:53:12 +0530

    My First Commit the code

---
+++ src/BAckOffice/Hello.java
@@ -0,0 +1,13 @@
+package BAckOffice;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("I am good ");
+		System.out.println("this is practice for git");
+
+	}
+
+}
---
+++ src/BAckOffice/Sum.java
@@ -0,0 +1,11 @@
+package BAckOffice;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		System.out.println("Practice Session");
+		// TODO Auto-generated method stub
+
+	}
+
+}
